26558d1631c6de2014b0e663b2c0283a7487eb28
vluv99 <v.luva99@gmail.com>
2025-01-29 23:53:59 +0100

    initial web scrape setup

--- eBookScraper/Program.cs
+++ eBookScraper/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Web;
 
-Console.WriteLine("Hello, World!");
+using System.IO;
+
+namespace eBookScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var htmlDocument = new HtmlDocument();
+            var filePath = "heaven-officials-blessing-0.html";
+            if (!File.Exists(filePath))
+            {
+                // send GET request to url
+                String url = "https://novelbin.me/novel-book/heaven-officials-blessing";
+                var httpClient = new HttpClient();
+                var html = httpClient.GetStringAsync(url).Result;
+                htmlDocument.LoadHtml(html);
+            
+                // Save to file
+                File.WriteAllText(filePath, html);  
+            }
+            else
+            {
+                // Read from file
+                string htmlText = File.ReadAllText(filePath);
+                htmlDocument.LoadHtml(htmlText);
+            }
+            
+            // Get the title
+            var titleElement = htmlDocument.DocumentNode.SelectSingleNode("//h3[@class='title']");
+            String title = HttpUtility.HtmlDecode(titleElement.InnerText).Trim();
+            Console.WriteLine("title: " + title);
+            
+            var infoBlockElement = htmlDocument.DocumentNode.SelectSingleNode("//ul[@class='info info-meta']");
+            
+            // Get author name
+            var authorElement = infoBlockElement.ChildNodes;
+            Console.WriteLine(authorElement);
+            
+            //String author = HttpUtility.HtmlDecode(titleElement.InnerText).Trim();
+            //Console.WriteLine("title: " + author);
+            
+            // Get completion status
+            
+            // Get image
+        }
+    }
+}
+
