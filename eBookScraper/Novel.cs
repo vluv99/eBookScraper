4e6397c5ce018bd5fcc98e701d57751292021f3c
vluv99 <v.luva99@gmail.com>
2025-02-04 18:44:20 +0100

    initial Novel class

---
+++ eBookScraper/Novel.cs
@@ -0,0 +1,12 @@
+namespace eBookScraper;
+
+class Novel
+{
+    public string title = "";
+    public string author = "";
+    public string[] altNames = [];
+    public string[] genres = [];
+    public string status = "";
+    public string description = "";
+    public string url = "";
+}
