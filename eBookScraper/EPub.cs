473dd9266f3b50809b2658b43f788cb929a99521
vluv99 <v.luva99@gmail.com>
2025-02-16 18:07:04 +0100

    Updated epub pages template strings

--- eBookScraper/EPub.cs
+++ eBookScraper/EPub.cs
@@ -17,64 +17,60 @@ public static class EPubClass
         var coverImageId = epub.AddImageData("cover.jpg", imageBytes);
         epub.AddMetaItem("cover", coverImageId);
 
-        var coverTemplate = """
-                            <!DOCTYPE html>
-                            			<html xmlns="http://www.w3.org/1999/xhtml">
-                            				<head>
-                            					<title>{0}</title>
-                            					<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-                            					<link type="text/css" rel="stylesheet" href="style.css"/>
-                            					<style type="text/css">
-                            						@page {
-                            							padding: 0;
-                            							margin: 0;
-                            						}
-                            					</style>
-                            				</head>
-                            				<body>
-                            					{1}
-                            				</body>
-                            			</html>
-                            """.Trim().Replace("\t", "");
+        var coverTemplate = $$"""
+                              <!DOCTYPE html>
+                              <html xmlns="http://www.w3.org/1999/xhtml">
+                                  <head>
+                                      <title>{{book.Title}}</title>
+                                      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                                      <link type="text/css" rel="stylesheet" href="style.css"/>
+                                      <style type="text/css">
+                                          @page {
+                                              padding: 0;
+                                              margin: 0;
+                                          }
+                                      </style>
+                                  </head>
+                                  <body>
+                                      {{book.Description}}
+                                  </body>
+                              </html>
+                              """;
 
         // cover
-        epub.AddXhtmlData("page-0.xhtml",
-            coverTemplate.Replace("{0}", book.Title).Replace("{1}", book.Description));
-        epub.AddNavPoint("Cover", "page-0.xhtml",
-            0);
+        epub.AddXhtmlData("page-0.xhtml", coverTemplate);
+        epub.AddNavPoint("Cover", "page-0.xhtml", 0);
 
         // chapter
-        var pageTemplate = """
-                           <!DOCTYPE html>
-                           			<html xmlns="http://www.w3.org/1999/xhtml">
-                           				<head>
-                           					<title>{0}</title>
-                           					<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-                           					<link type="text/css" rel="stylesheet" href="style.css"/>
-                           					<style type="text/css">
-                           						@page {
-                           							padding: 0;
-                           							margin: 0;
-                           						}
-                           					</style>
-                           				</head>
-                           				<body>
-                           					{1}
-                           				</body>
-                           			</html>
-                           """.Trim().Replace("\t", "");
         for (var index = 0; index < book.Chapters.Count; index++)
         {
             var name = $"page-{index + 1}.xhtml";
             var chapter = book.Chapters[index];
+            var content = chapter.Content.Select(c => c.Replace("<br>", "<br/>"));
 
-            var content = chapter.Content
-                .Select(c => c.Replace("<br>", "<br/>"));
+            var chapterTemplate = $$"""
+                                    <!DOCTYPE html>
+                                    <html xmlns="http://www.w3.org/1999/xhtml">
+                                        <head>
+                                            <title>{{chapter.Title}}</title>
+                                            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                                            <link type="text/css" rel="stylesheet" href="style.css"/>
+                                            <style type="text/css">
+                                                @page {
+                                                    padding: 0;
+                                                    margin: 0;
+                                                }
+                                            </style>
+                                        </head>
+                                        <body>
+                                            {{string.Join(" ", content)}}
+                                        </body>
+                                    </html>
+                                    """;
 
-            epub.AddXhtmlData(name,
-                pageTemplate.Replace("{0}", chapter.Title).Replace("{1}", string.Join(" ", content)));
-            epub.AddNavPoint(chapter.Title, name,
-                index + 1);
+
+            epub.AddXhtmlData(name, chapterTemplate);
+            epub.AddNavPoint(chapter.Title, name, index + 1);
         }
 
         var path = Fetcher.SavePath + "/" + book.Title + ".epub";
